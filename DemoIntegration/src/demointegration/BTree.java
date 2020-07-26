9498240b2e446abe087b22505525eb2cad171734
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-26 20:20:48 +0530

    Update BTree.java

--- DemoIntegration/src/demointegration/BTree.java
+++ DemoIntegration/src/demointegration/BTree.java
@@ -25,7 +25,7 @@ public class BTree {
         boolean isSymbol = false;
         for(int i=0; i<RE.length();i++)
         {
-            if(RE.charAt(i)=='/' || RE.charAt(i)=='+'){
+            if(RE.charAt(i)=='/' || RE.charAt(i)=='&' || RE.charAt(i)=='*'){
                 isSymbol = true;
             }
             if(isSymbol)
