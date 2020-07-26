0b1b63dd31a6fdb6fe9538338184fedc67751d59
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-26 20:19:01 +0530

    Update FiniteAutomata.java

--- DemoIntegration/src/demointegration/FiniteAutomata.java
+++ DemoIntegration/src/demointegration/FiniteAutomata.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package demointegration;
 
 import java.util.*;
 
-/**
- *
- * @author Sreharine Dhileeban
- */
+
 public class FiniteAutomata {
     int states;
     List<Character> inputs;
